c0105428462cf22f437a865942fddfdd800cd1ab
Victor Santet <Victor Santet>
2015-12-15 20:25:41 +0000

    start: add the third row of pions

--- app/src/main/java/com/mayer/lucas/draughtgame/Checkers.java
+++ app/src/main/java/com/mayer/lucas/draughtgame/Checkers.java
@@ -53,9 +53,9 @@ public class Checkers implements Game {
             plate[i] = new Piece[size];
             for (int j = 0; j < size; ++j)
                 if ((i + j) % 2 == 0)
-                    if (j < 2)
+                    if (j < 3)
                         plate[i][j] = Piece.WPawn;
-                    else if (j >= size - 2)
+                    else if (j >= size - 3)
                         plate[i][j] = Piece.BPawn;
                     else
                         plate[i][j] = Piece.Empty;
